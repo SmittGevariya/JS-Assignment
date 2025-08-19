1e73b81cfa97f6bf7d33a91c9f1a1af5f56340ed
Smit Gevariya <114725603+SmittGevariya@users.noreply.github.com>
2025-08-19 16:38:37 +0530

    Feat: Add function to sum numbers from a string

--- Task2.js
+++ Task2.js
@@ -10,6 +10,4 @@ function sumOfStringNumbers(str) {
 }
 
 const input = "1.5, 2.3, 3.1, 4, 5.5, 6, 7, 8, 9, 10.9";
-console.log(sumOfStringNumbers(input));  
-
-        
+console.log(sumOfStringNumbers(input));          
